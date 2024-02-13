9a6b68fc0157269dcc94040e5745ea980e54b6c5
juan-barbosa <jesbangtr@gmail.com>
2024-02-12 19:23:46 -0500

    v2

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respuestas.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://answers_75sn_user:bvt40I0RXHnBNYWkicUknthHsHow7T4H@dpg-cn592hect0pc738efmk0-a.oregon-postgres.render.com/answers_75sn'
 db = SQLAlchemy(app)
 
 
