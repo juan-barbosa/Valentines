499773dd87b01ba94afb7ab0a47e707081472003
juan-barbosa <jesbangtr@gmail.com>
2024-02-13 22:32:46 -0500

    V3

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import request, render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for, jsonify
 from app import app, db
 from app.models import Respuesta
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('disclaimer.html')
 
 @app.route('/invitacion', methods=['POST'])
 def invitacion():
@@ -12,4 +12,8 @@ def invitacion():
     nueva_respuesta = Respuesta(respuesta=respuesta_usuario)
     db.session.add(nueva_respuesta)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({"message": "Respuesta guardada exitosamente"})
+
+@app.route('/question')
+def question():
+    return render_template('question.html')
--- app/static/script.js
+++ app/static/script.js
@@ -1,1 +1,60 @@
+var mensajes = ["Oh my bad, maybe 'Yes' is too small.. Fixed :3", "Huh?.. 🤨", "No problem, you've got another chance!", "Maybe wanna change your mind??", "Seriously? 5 times?", "Good thing you can think it again", "Very funny :3", "Running out of messages!", "If you got here let me tell you, you are absolutely gorgeous :3", "I think the 'YES' button is big enough now??", "Messages will just repeat after this one :((("]; // Define aquí tus mensajes en el orden deseado
+var mensajeIndex = 0;
+document.getElementById("but-yes").addEventListener("click", function() {
+    mostrarRespuesta();
+    enviarRespuesta("Yes");
+});
 
+document.getElementById("but-no").addEventListener("click", function() {
+    mostrarMensaje();
+    aumentarTamanioBoton();
+    enviarRespuesta("No");
+});
+
+function enviarRespuesta(respuesta) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/invitacion", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send("respuesta=" + respuesta);
+}
+
+function mostrarMensaje() {
+    var randomRotation = Math.random() * 90 - 45; // Rotación aleatoria entre -45 y 45 grados
+
+    var mensajeContainer = document.getElementById("mensajeContainer");
+    var nuevoMensaje = document.createElement("div");
+    nuevoMensaje.innerText = mensajes[mensajeIndex];
+    nuevoMensaje.style.position = "absolute";
+    nuevoMensaje.style.left = Math.random() * (window.innerWidth - 200) + "px";
+    nuevoMensaje.style.top = Math.random() * (window.innerHeight - 50) + "px";
+    nuevoMensaje.style.transform = "rotate(" + randomRotation + "deg)";
+    nuevoMensaje.style.zIndex = 5;
+    mensajeContainer.appendChild(nuevoMensaje);
+
+    mensajeIndex = (mensajeIndex + 1) % mensajes.length; // Avanza al siguiente mensaje en orden
+}
+
+function mostrarRespuesta() {
+    document.getElementById("mensajeContainer").style.display = "none"; // Oculta el contenedor de mensajes
+    document.getElementById("but-cont-q").style.display="none";
+    document.getElementById("pregunta-1").style.display="none";
+    document.getElementById("xd").style.display="none";
+    document.getElementById("respuestaContainer").style.display = "block";
+    document.getElementById("respuestaContainer").style.color = "brown";
+    document.getElementById("respuestaContainer").style.top = "-10%"; // Muestra el contenedor de respuesta
+    
+}
+function aumentarTamanioBoton() {
+    var botonYes = document.getElementById("but-yes");
+    var estilo = window.getComputedStyle(botonYes);
+    var tamanioActual = parseInt(estilo.getPropertyValue("font-size"));
+    botonYes.style.fontSize = (tamanioActual + 20) + "px";
+    var tamanioacctual = parseInt(estilo.getPropertyValue("padding"));
+    botonYes.style.padding = (tamanioacctual + 20) + "px";
+    
+}
